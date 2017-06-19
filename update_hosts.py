df29e903b2221212d1ff0d8042a0a531b8f11e17
shenbo <shenbo@hotmail.com>
2017-06-19 11:50:14 +0800

    Signed-off-by: shenbo <shenbo@hotmail.com>

--- update_hosts.py
+++ update_hosts.py
@@ -13,50 +13,45 @@ import sys
 import requests
 import time
 
-# kk hosts
 kk_hosts = '# kk hosts start\n' \
            '172.18.1.30 ekp.aerosun.cn\n' \
            '172.18.1.32 kk.aerosun.cn\n' \
-           '# kk hosts end\n\n\n'
-# ipv4 hosts url
+           '# kk hosts end\n\n'
+
 ipv4_url = 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts'
-# windows hosts file dir
+
 hosts_file_dir = 'C:/Windows/System32/drivers/etc/hosts'
 desktop_dir = 'C:/Users/shenbo/Desktop/hosts'
 
-print('loading hosts file...\n')
 
-try:
-    req = requests.get(ipv4_url)
-    ipv4_hosts = req.content.decode(encoding='utf-8')
-    print('got ipv4 hosts!\n')
+def gethosts():
+    print('loading hosts file...\n')
+    try:
+        req = requests.get(ipv4_url)
+        ipv4_hosts = req.content.decode(encoding='utf-8')
+        print('got ipv4 hosts!\n')
+    except requests.HTTPError as e:
+        print(e)
+        input()
 
-except requests.HTTPError as e:
-    print(e)
-    raw_input()
+    now = time.localtime(time.time())
+    tm = time.strftime('%Y-%m-%d, %H:%M:%S', now)
+    s = '\n\n##hosts updated @ ' + tm
 
-t = time.strftime('%Y-%m-%d, %H:%M:%S', time.localtime(time.time()))
+    hosts = kk_hosts + ipv4_hosts + s
+    return hosts
 
-hosts = kk_hosts + ipv4_hosts + '\n\n##hosts updated @ ' + t
-print(hosts)
 
-try:
-    fd = os.open(hosts_file_dir, os.O_RDWR | os.O_CREAT)
+def updatehosts(hosts):
+    with open(hosts_file_dir, 'wb') as fd:
+        fd.write(bytes(hosts, 'UTF-8'))
 
-    ret = os.write(fd, bytes(hosts, 'UTF-8'))
-    print(ret)
-    os.close(fd)
+    with open(desktop_dir, 'wb') as fd:
+        fd.write(bytes(hosts, 'UTF-8'))
 
-    input('hosts updated @ ' + t + ' !\npress enter!')
+    input('hosts updated succeed !\npress enter!')
 
-except IOError as e:
-    print(e)
 
-finally:
-    fd = os.open(desktop_dir, os.O_RDWR | os.O_CREAT)
-
-    ret = os.write(fd, bytes(hosts, 'UTF-8'))
-    print(ret)
-    os.close(fd)
-
-    input('the host file has been saved on your desktop!')
+if __name__ == '__main__':
+    h = gethosts()
+    updatehosts(h)
