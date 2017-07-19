49b1e4f21a5bd16d60f3a676621336d71dbc9c48
shenbo <shenbo@hotmail.com>
2017-07-19 17:54:34 +0800

    Signed-off-by: shenbo <shenbo@hotmail.com>

--- 03_magic_squre/ms.py
+++ 03_magic_squre/ms.py
@@ -1,84 +1,67 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import itertools
+import numpy as np
 
-"""
-define a 03_magic_squre: 4*4 array[][]
-    | a[0][0] | a[0][1] | a[0][2] | a[0][3] |
-    | a[1][0] | a[1][1] | a[1][2] | a[1][3] |
-    | a[2][0] | a[2][1] | a[2][2] | a[2][3] |
-    | a[3][0] | a[3][1] | a[3][2] | a[3][3] |
-
-pick 7 number in range(1,17) as a 03_magic_squre seed list: seed[0]~seed[6].
-so:
-    a[0][0] = seed[0]
-    a[0][1] = seed[1]
-    a[0][2] = seed[2]
-    a[0][3] = 34-seed[0]-seed[1]-seed[2]
-
-    a[1][0] = seed[3]
-    a[1][1] = seed[4]
-    a[1][2] = seed[5]
-    a[1][3] = 34-seed[3]-seed[4]-seed[5]
-
-    a[2][0] = seed[6]
-    a[2][1] = 2*seed[0]+seed[1]+seed[2]+seed[3]-seed[5]+seed[6]-34
-    a[2][2] = 68-seed[1]-seed[2]-seed[3]-seed[4]-seed[6]-2*seed[0]
-    a[2][3] = seed[4]+seed[5]-seed[6]
-
-    a[3][0] = 34-seed[3]-seed[6]-seed[0]
-    a[3][1] = seed[5]-2*seed[1]-seed[2]-seed[3]-seed[4]-2*seed[0]-seed[6]+68
-    a[3][2] = 2*seed[0]+seed[1]+seed[3]+seed[4]-seed[5]+seed[6]-34
-    a[3][3] = seed[0]+seed[1]+seed[2]+seed[3]+seed[6]-34
-
-see if a[][] is a 03_magic_squre.
-"""
-def getms(seed):
-    """get a magic squre.
-    para:
-        seed -- a list of 7 int number.
-    return:
-        ms_list -- magic squre list.
-    """
-    ms_list = []
-    # a00 ~ a03
-    ms_list.append(seed[0])
-    ms_list.append(seed[1])
-    ms_list.append(seed[2])
-    ms_list.append(34 - seed[0] - seed[1] - seed[2])
-    # a10 ~ a13
-    ms_list.append(seed[3])
-    ms_list.append(seed[4])
-    ms_list.append(seed[5])
-    ms_list.append(34 - seed[3] - seed[4] - seed[5])
-    # a20 ~ a23
-    ms_list.append(seed[6])
-    ms_list.append(2 * seed[0] + seed[1] + seed[2] + seed[3] - seed[5] + seed[6] - 34)
-    ms_list.append(68 - seed[1] - seed[2] - seed[3] - seed[4] - seed[6] - 2 * seed[0])
-    ms_list.append(seed[4] + seed[5] - seed[6])
-    # a30 ~ a33
-    ms_list.append(34 - seed[3] - seed[6] - seed[0])
-    ms_list.append(seed[5] - 2 * seed[1] - seed[2] - seed[3] - seed[4] - 2 * seed[0] - seed[6] + 68)
-    ms_list.append(2 * seed[0] + seed[1] + seed[3] + seed[4] - seed[5] + seed[6] - 34)
-    ms_list.append(seed[0] + seed[1] + seed[2] + seed[3] + seed[6] - 34)
-    return ms_list
+import timeit
 
 
-def isms(ms_list):
-    """if array is a magic squre, return 1"""
-    if len(set(ms_list)) != 16:
+def gen_ms(sd):
+    ms_array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    ms_array[0] = sd[0]
+    ms_array[1] = sd[1]
+    ms_array[2] = sd[2]
+    ms_array[3] = 34 - sd[0] - sd[1] - sd[2]
+
+    ms_array[4] = sd[3]
+    ms_array[5] = sd[4]
+    ms_array[6] = sd[5]
+    ms_array[7] = - sd[3] - sd[4] - sd[5] + 34
+
+    ms_array[8] = sd[6]
+    ms_array[9] = 2 * sd[0] + sd[1] + sd[2] + sd[3] - sd[5] + sd[6] - 34
+    ms_array[10] = - 2 * sd[0] - sd[1] - sd[2] - sd[3] - sd[4] - sd[6] + 68
+    ms_array[11] = sd[4] + sd[5] - sd[6]
+
+    ms_array[12] = - sd[0] - sd[3] - sd[6] + 34
+    ms_array[13] = - 2 * sd[0] - 2 * sd[1] - sd[2] - sd[3] - sd[4] + sd[5] - sd[6] + 68
+    ms_array[14] = 2 * sd[0] + sd[1] + sd[3] + sd[4] - sd[5] + sd[6] - 34
+    ms_array[15] = sd[0] + sd[1] + sd[2] + sd[3] + sd[6] - 34
+
+    return ms_array
+
+
+def gen_ms_mat(sd):
+    xx = np.append(np.array(sd), [1])
+    yy = np.array([[1, 0, 0, -1, 0, 0, 0, 0, 0, 2, -2, 0, -1, -2, 2, 1],
+                   [0, 1, 0, -1, 0, 0, 0, 0, 0, 1, -1, 0, 0, -2, 1, 1],
+                   [0, 0, 1, -1, 0, 0, 0, 0, 0, 1, -1, 0, 0, -1, 0, 1],
+                   [0, 0, 0, 0, 1, 0, 0, -1, 0, 1, -1, 0, -1, -1, 1, 1],
+                   [0, 0, 0, 0, 0, 1, 0, -1, 0, 0, -1, 1, 0, -1, 1, 0],
+                   [0, 0, 0, 0, 0, 0, 1, -1, 0, -1, 0, 1, 0, 1, -1, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, -1, -1, -1, -1, 1, 1],
+                   [0, 0, 0, 34, 0, 0, 0, 34, 0, -34, 68, 0, 34, 68, -34, -34]])
+    ms_array = np.matmul(xx, yy)
+    return ms_array
+
+
+def isms(ms_array):
+    if len(set(ms_array)) != 16:
         return
-    for k in ms_list:
+    for k in ms_array:
         if k > 16 or k < 1:
             return
     return 1
 
-# itertools.permutations(iterable[, r])
-# return successive r length permutations of elements in the iterable.
-PERM = itertools.permutations(xrange(1, 17), 7)
 
-for i in range(0, 60000000):
-    seed_list = PERM.next()
-    magic_squre_list = getms(seed_list)
-    if isms(magic_squre_list):
-        print i, magic_squre_list
+def test():
+    PERM = itertools.permutations(range(1, 17), 7)
+    for i in PERM:
+        magic_squre = gen_ms(i)
+        if isms(magic_squre):
+            print(i, magic_squre)
+
+
+if __name__ == '__main__':
+    t1 = timeit.Timer('test()', 'from __main__ import test')
+    print(t1.timeit(1))
+
