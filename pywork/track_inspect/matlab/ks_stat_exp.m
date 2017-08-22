ff45322c536e1c2ff6b47b915846d26a67425777
shenbo <shenbo@hotmail.com>
2017-08-22 22:41:27 +0800

    d
    d

--- pywork/track_inspect/matlab/ks_stat_exp.m
+++
@@ -1,9 +0,0 @@
-function [D] = ks_stat_exp(x, a)
-% Compute Kolmogorv Smirnov statistic for testing wether x comes from
-% distribution Exp(a)
-
-cdf_x = (0.5:length(x)-0.5)'/length(x);
-cdf_a = 1-exp(-x/a);
-D = max(abs(cdf_x-cdf_a));
-
-end
--- pywork/track_inspect/matlab/test_parse.m
+++
@@ -1,2 +0,0 @@
-fname = scores_file_name(0, 1);
-[scores, tieIndex, gt] = read_scores_file(fname);
